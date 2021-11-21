fec621bc644b88a2bf5798f86f9516a2a68c0d90
Syyrion <cuiricky4@gmail.com>
2021-11-21 14:31:37 -0500

    First iteration utils.lua

--- Scripts/utils.lua
+++ Scripts/utils.lua
@@ -2,81 +2,55 @@ u_execDependencyScript("ohvrvanilla", "base", "vittorio romeo", "utils.lua")
 
 -- Constants
 THICKNESS = 40			-- Wall thickness. Sometimes more convenient to define in utils
-FOCUSRATIO = 0.625		-- The percentage by which the player shrinks when focused
+FOCUS_RATIO = 0.625		-- The percentage by which the player shrinks when focused
+PLAYER_WIDTH_UNFOCUSED = 23
+PLAYER_WIDTH_FOCUSED = PLAYER_WIDTH_UNFOCUSED * FOCUS_RATIO
 
 
-function math.fastsin_taylor(x)
-	x = (x - math.pi) % math.tau - math.pi
-	return x - x ^ 3 / 6 + x ^ 5 / 120 - x ^ 7 / 5040 + x ^ 9 / 362880
-end
-
-function math.fastcos_taylor(x)
-	x = (x - math.pi) % math.tau - math.pi
-	return 1 - x ^ 2 / 2 + x ^ 4 / 24 - x ^ 6 / 720 + x ^ 8 / 40320
-end
-
-
--- Tests whether a number is equal to any number within a table
-function equalsWithin(num, arr)
-	for i = 1, #arr do
-		if num == arr[i] then return true end
-	end
-	return false
-end
-
--- Takes a coordinate, translates it by dX and dY, and returns the new coordinates
-function transformTranslate(dX, dY, x, y)
-	return x + dX, y + dY
+-- Tests whether a table contains a specific value on any existing key
+function tableContainsValue(val, table)
+    for _, v in pairs(table) do
+        if val == v then return true end
+    end
+    return false
 end
 
 -- Takes a coordinate, rotates it by R radians about the origin, and returns the new coordinates
-function transformRotation(R, x, y)
-	return x * math.cos(R) - y * math.sin(R), x * math.sin(R) + y * math.cos(R)
+function rotate2DPointAroundOrigin(R, x, y)
+    local cos, sin = math.cos(R), math.sin(R)
+    return x * cos - y * sin, x * sin + y * cos
 end
-
--- Takes a coordinate, scales it by sX or sY across the x or y axis respectively, and returns the new coordinates
-function transformScale(sX, sY, x, y)
-	return x * sY, x * sX
-end
-
--- Transformation functions can be chained
--- Ex: transformTranslate(20, 10, transformRotation(math.pi, x, y))
 
 -- Sets hue to a specific value by setting its min an max to the same value
-function s_setHue(h)
-	s_setHueMin(h)
-	s_setHueMax(h)
+function forceSetHue(h)
+    s_setHueMin(h)
+    s_setHueMax(h)
 end
 
 -- Sets pulse to a specific value by setting its min an max to the same value
-function s_setPulse(p)
-	s_setPulseMin(p)
-	s_setPulseMax(p)
-end
-
--- Sign function
-function math.sgn(x)
-	return x > 0 and 1 or x == 0 and 0 or -1
+function forceSetPulse(p)
+    s_setPulseMin(p)
+    s_setPulseMax(p)
 end
 
 -- Square wave function with period p at value x with duty cycle d (range [-1, 1])
-function math.square(x, p, d)
-	return sgn(math.sin(math.pi * (2 * x / p + 0.5 - d)) - math.cos(math.pi * d))
+function squareWave(x, p, d)
+	return getSign(math.sin(math.pi * (2 * x / p + 0.5 - d)) - math.cos(math.pi * d))
 end
 
 -- Triangle wave function with period p at value x (range [-1, 1])
-function math.triangle(x, p)
+function triangleWave(x, p)
 	return math.asin(math.sin(math.tau * x / p)) * 2 / math.pi
 end
 
 -- Sawtooth wave function with period p at value x (range [-1, 1])
-function math.sawtooth(x, p)
+function sawtoothWave(x, p)
 	return 2 * (x / p - math.floor(0.5 + x / p))
 end
 
 -- Takes a value <i> between <a> and <b> and proportionally maps it to a value between <c> and <d>
-function math.map(i, a, b, c, d)
-	return lerp(c, d, inverseLerp(a, b, i))
+function mapValue(i, a, b, c, d)
+    return c + ((d - c) / (b - a)) * (i - a)
 end
 
 -- Guarantees an input value to be a valid number of sides. Falls back to the level's current number of sides if an invalid argument is given
@@ -92,50 +66,50 @@ function fromHSV(h, s, v)
 end
 
 -- Distance from the center to the player position
-function getPlayerRadius()
-	return l_getRadiusMin() * l_getPulse() / l_getPulseMin() + l_getBeatPulse()
+function getDistanceBetweenCenterAndPlayer()
+    return l_getRadiusMin() * l_getPulse() / l_getPulseMin() + l_getBeatPulse()
 end
 -- Distance from center to tip of player arrow
-function getPlayerTipRadius()
-	return getPlayerRadius() + 7.25
+function getDistanceBetweenCenterAndPlayerTip()
+    return getDistanceBetweenCenterAndPlayer() + 7.25
 end
 
 -- Distance from center to base of player arrow (depends on focus)
-function getPlayerBaseRadius(mFocus)
-	return getPlayerRadius() - (mFocus and 1.265625 or 2.025)
+function getDistanceBetweenCenterAndPlayerBase(mFocus)
+    return getDistanceBetweenCenterAndPlayer() - 2.025 * (mFocus and FOCUS_RATIO or 1)
 end
 
 -- Distance from the base to the tip of the player triangle (depends on focus)
 function getPlayerHeight(mFocus)
-	return 7.25 + (mFocus and 1.265625 or 2.025)
-end
-
--- Half of the base width of the player triangle (depends on focus)
-function getPlayerHalfBaseWidth(mFocus)
-	return mFocus and 11.5 or 7.1875
+    return 7.25 + 2.025 * (mFocus and FOCUS_RATIO or 1)
 end
 
 -- Base width of the player triangle (depends on focus)
 function getPlayerBaseWidth(mFocus)
-	return mFocus and 23 or 14.375
+    return mFocus and PLAYER_WIDTH_FOCUSED or PLAYER_WIDTH_UNFOCUSED
+end
+
+-- Half of the base width of the player triangle (depends on focus)
+function getPlayerHalfBaseWidth(mFocus)
+    return getPlayerBaseWidth(mFocus) * 0.5
 end
 
 -- Radius of a circle circumscribed around the center polygon cap
 function getCapRadius()
-	return getPlayerRadius() * 0.75
+    return getDistanceBetweenCenterAndPlayer() * 0.75
 end
 
 -- Width of the polygon border
-function getPolygonBorderWidth()
-	return 5
+function getPivotBorderWidth()
+    return 5
 end
 
 -- Radius of a circle circumscribed around the center polygon
-function getPolygonRadius()
-	return getCapRadius() + getPolygonBorderWidth()
+function getPivotRadius()
+    return getCapRadius() + getPivotBorderWidth()
 end
 
 -- Returns the speed of walls in units per frame (5 times the speed mult)
-function getWallSpeed()
-	return l_getSpeedMult() * 5
+function getWallSpeedInUnitsPerFrame()
+    return l_getSpeedMult() * (u_getDifficultyMult() ^ 0.65) * 5
 end
